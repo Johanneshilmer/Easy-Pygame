f6d57545285e88df9b17b7a646ded683de55f951
Johannes Hilmer <johanneshilmerson94@gmail.com>
2023-03-27 21:53:19 +0200

    move

--- main.py
+++ main.py
@@ -22,6 +22,26 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+        
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+            
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
 
 player = Player()
 pygame.init()
@@ -41,11 +61,17 @@ while running:
         elif event.type == QUIT:
             running = False
 
+    # Get the set of keys pressed and check for user input
+    pressed_keys = pygame.key.get_pressed()
+    
+    #Update "movment" from class player.
+    player.update(pressed_keys)
+    
     # Fill the screen with black
     screen.fill((0, 0, 0))
 
     # Draw the player on the screen
-    screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    screen.blit(player.surf, player.rect)
 
     # Update the display
     pygame.display.flip()
